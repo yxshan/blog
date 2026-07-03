7bc4ce6ed2fff26abdd1b0d0c144b902e22c6ab1
江林 <1851114089@qq.com>
2026-07-03 14:20:46 +0800

    fix: scroll to top on route change

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route } from "react-router";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router";
+import { useEffect } from "react";
 import { HelmetProvider } from "react-helmet-async";
 import { ThemeProvider } from "./features/theme/ThemeProvider";
 import { BlogLayout } from "./layouts/BlogLayout";
@@ -6,11 +7,21 @@ import Home from "./pages/Home";
 import Post from "./pages/Post";
 import { NotFound } from "./pages/NotFound";
 
+/** 路由切换时自动滚动到页面顶部 */
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
+}
+
 function App() {
   return (
     <HelmetProvider>
       <ThemeProvider>
         <BrowserRouter basename="/blog">
+          <ScrollToTop />
           <Routes>
             <Route element={<BlogLayout />}>
               <Route index element={<Home />} />
